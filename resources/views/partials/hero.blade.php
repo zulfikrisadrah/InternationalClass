ec86c1b668b9efd000a495f20a5e9895adf7c127
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-11-03 04:14:29 +0800

    style: change height hero section

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,5 +1,5 @@
 <!-- Hero Section -->
-<section class="min-h-screen flex flex-col justify-end bg-cover bg-center text-white" style="background-image: url('images/hero.png');">
+<section class="flex flex-col justify-end bg-cover bg-center text-white" style="background-image: url('images/hero.png'); height: calc(100vh - 110px);">
     <div class="container mx-auto px-6 pb-12">
         <h1 class="text-5xl font-semibold">International Class</h1>
         <p class="mt-4 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.

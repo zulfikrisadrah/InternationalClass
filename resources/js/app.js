23238d801d00580ede11b92fc785878ae7d3d144
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-11-26 14:26:27 +0800

    feat: add aos and animation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
     import './bootstrap';
 
     import Alpine from 'alpinejs';
+    import AOS from 'aos';
+    import 'aos/dist/aos.css'; // Import file CSS AOS
 
+    AOS.init(); // Inisialisasi AOS
     window.Alpine = Alpine;
 
     Alpine.start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,20 @@ export default {
                 primary: '#0F1649',
                 info: '#084D9C',
             },
+            keyframes: {
+                slideIn: {
+                    '0%': { transform: 'translateX(-100%)', opacity: '0' },
+                    '100%': { transform: 'translateX(0)', opacity: '1' },
+                },
+                fadeIn: {
+                    '0%': { opacity: '0' },
+                    '100%': { opacity: '1' },
+                },
+            },
+            animation: {
+                'slide-in': 'slideIn 1.2s ease-out',
+                'fade-in': 'fadeIn 1.5s ease-in',
+            },
         },
     },
 

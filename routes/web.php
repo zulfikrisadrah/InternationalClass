2630703a8c2f0a38c340025f219a8178ca533e80
andi muh haikal lukman <113303662+itsdccal@users.noreply.github.com>
2024-11-26 14:27:42 +0800

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ClassController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\StaffDashboardController;
 use App\Http\Controllers\StudentDashboardController;
@@ -26,61 +28,34 @@ Route::get('/IE', function () {
 Route::get('/studyProgram', function () {
     return view('studyProgram');
 });
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// });
-
-// Admin Dashboard Resource
-// Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
-//     Route::get('dashboard/admin', [AdminDashboardController::class, 'index'])->name('dashboard.admin.index');
-// });
-
-// Route::middleware(['auth', 'verified', 'role:staff'])->group(function () {
-//     Route::get('dashboard/staff', [StaffDashboardController::class, 'index'])->name('dashboard.staff.index');
-// });
-
-// Route::middleware(['auth', 'verified', 'role:student'])->group(function () {
-//     Route::get('dashboard/student', [StudentDashboardController::class, 'index'])->name('dashboard.student.index');
-// });
-
-
 
 Route::get('/dashboard', function () {
     return view('dashboard.admin.home');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/dashboard/class', function () {
-    return view('dashboard.admin.class');
-})->middleware(['auth', 'verified'])->name('class');
-
-Route::get('/dashboard/program', function () {
-    return view('dashboard.admin.program');
-})->middleware(['auth', 'verified'])->name('program');
-
-Route::get('/dashboard/information', function () {
-    return view('dashboard.admin.information');
-})->middleware(['auth', 'verified'])->name('information');
-
-Route::get('/dashboard/user', function () {
-    return view('dashboard.admin.user');
-})->middleware(['auth', 'verified'])->name('user');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route:: prefix('admin')->name('admin.')->group(function () {
-        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-
+    Route::prefix('admin')->name('admin.')->group(function () {
         Route::middleware('can:manage class')->group(function () {
-            Route::get('/news', [ClassController::class, 'news'])->name('admin.news');
+            Route::resource('class', ClassController::class);
         });
 
         Route::middleware('can:manage news')->group(function () {
-            Route::get('/news', [NewsController::class, 'news'])->name('admin.news');
+            Route::resource('news', NewsController::class);
         });
+        Route::middleware('can:manage user')->group(function () {
+            Route::resource('user', UserController::class);
+        });
+        Route::middleware('can:manage program')->group(function () {
+            Route::resource('program', ProgramController::class);
+        });
+    });
+    Route::prefix('student')->name('student.')->group(function () {
+
     });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';

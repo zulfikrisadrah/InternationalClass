15a37ed67c167cc5ec410a305c8dd617ce7e0498
Raehanrh <113957089+raehanramadhanhamzah@users.noreply.github.com>
2024-12-09 19:33:46 +0800

    feat: add study program description seeder

--- database/seeders/StudyProgramSeeder.php
+++ database/seeders/StudyProgramSeeder.php
@@ -16,67 +16,63 @@ class StudyProgramSeeder extends Seeder
             [
                 'study_program_Name' => 'PENDIDIKAN DOKTER - S1',
                 'ID_Faculty' => 3,
-                'image' => 'images/studyprogram/FK-unhas.png',
+                'study_program_Image' => 'images/studyprogram/FK-unhas.png',
+                'study_program_Description' =>
+                    "The Medical Education program offers comprehensive training in medicine with a curriculum 
+                    designed to develop competent and professional medical practitioners. Students are equipped 
+                    with the knowledge, skills, and attitudes necessary to address healthcare challenges. 
+                    The program includes rigorous theoretical studies combined with hands-on clinical training 
+                    in hospitals and healthcare facilities.",
             ],
             [
                 'study_program_Name' => 'PENDIDIKAN DOKTER GIGI - S1',
                 'ID_Faculty' => 10,
-                'image' => 'images/studyprogram/doktergigi.png',
+                'study_program_Image' => 'images/studyprogram/doktergigi.png',
+                'study_program_Description' =>
+                    "The Dentistry Education program focuses on developing students into skilled dental professionals 
+                    who can diagnose, treat, and prevent oral and dental diseases. The curriculum combines academic 
+                    excellence with clinical practice to ensure students are well-prepared to provide high-quality 
+                    oral healthcare services.",
             ],
             [
                 'study_program_Name' => 'ILMU KEPERAWATAN - S1',
                 'ID_Faculty' => 15,
-                'image' => 'images/studyprogram/keperawatan.png',
+                'study_program_Image' => 'images/studyprogram/keperawatan.png',
+                'study_program_Description' =>
+                    "The Nursing Science program is dedicated to training compassionate and competent nurses who can 
+                    provide comprehensive care to individuals, families, and communities. The program emphasizes 
+                    a strong foundation in health sciences, nursing ethics, and practical experiences in various 
+                    healthcare settings.",
             ],
             [
                 'study_program_Name' => 'KESEHATAN MASYARAKAT - S1',
                 'ID_Faculty' => 11,
-                'image' => 'images/studyprogram/kesmas.png',
+                'study_program_Image' => 'images/studyprogram/kesmas.png',
+                'study_program_Description' =>
+                    "The Public Health program aims to educate future public health leaders who can tackle global 
+                    health challenges. The curriculum covers a wide range of topics, including epidemiology, health 
+                    promotion, environmental health, and health policy. Students will develop the skills needed 
+                    to improve population health through innovative solutions.",
             ],
             [
                 'study_program_Name' => 'TEKNIK SIPIL - S1',
                 'ID_Faculty' => 4,
-                'image' => 'images/studyprogram/sipil.png',
+                'study_program_Image' => 'images/studyprogram/sipil.png',
+                'study_program_Description' =>
+                    "The Civil Engineering program provides a robust education in designing, constructing, and 
+                    maintaining infrastructure. Students will learn about structural analysis, geotechnical 
+                    engineering, and sustainable construction practices, preparing them to contribute to urban 
+                    development and infrastructure projects.",
             ],
             [
                 'study_program_Name' => 'TEKNIK INFORMATIKA - S1',
                 'ID_Faculty' => 4,
-                'image' => 'images/studyprogram/infor.png',
-            ],
-            [
-                'study_program_Name' => 'TEKNIK ARSITEKTUR - S1',
-                'ID_Faculty' => 4,
-                'image' => 'images/studyprogram/arsitektur.png',
-            ],
-            [
-                'study_program_Name' => 'TEKNIK GEOLOGI - S1',
-                'ID_Faculty' => 4,
-                'image' => 'images/studyprogram/geo.png',
-            ],
-            [
-                'study_program_Name' => 'ILMU HUKUM - S1',
-                'ID_Faculty' => 2,
-                'image' => 'images/studyprogram/FH-UNHAS-10.png',
-            ],
-            [
-                'study_program_Name' => 'ILMU HUBUNGAN INTERNASIONAL - S1',
-                'ID_Faculty' => 5,
-                'image' => 'images\studyprogram\hi.png',
-            ],
-            [
-                'study_program_Name' => 'ILMU KOMUNIKASI - S1',
-                'ID_Faculty' => 5,
-                'image' => 'images\studyprogram\ilkom.png',
-            ],
-            [
-                'study_program_Name' => 'MANAJEMEN - S1',
-                'ID_Faculty' => 1,
-                'image' => 'images\studyprogram\Fakultas-Ekonomi-dan-Bisnis.png',
-            ],
-            [
-                'study_program_Name' => 'AKUTANSI - S1',
-                'ID_Faculty' => 1,
-                'image' => 'images\studyprogram\Fakultas-Ekonomi-dan-Bisnis.png',
+                'study_program_Image' => 'images/studyprogram/infor.png',
+                'study_program_Description' =>
+                    "The Informatics Engineering program is designed to prepare students for careers in the ever-evolving 
+                    field of information technology. With a strong focus on software development, artificial intelligence, 
+                    and cybersecurity, the program equips graduates with the technical expertise and problem-solving 
+                    skills needed to excel in the tech industry.",
             ],
         ];
 
@@ -87,9 +83,9 @@ class StudyProgramSeeder extends Seeder
             if ($faculty) {
                 DB::table('study_programs')->insert([
                     'study_program_Name' => $program['study_program_Name'],
-                    'study_program_Image' => $program['image'],
+                    'study_program_Image' => $program['study_program_Image'],
                     'degree' => 'Undergraduate',
-                    'study_program_Description' => null,
+                    'study_program_Description' => $program['study_program_Description'],
                     'International_Accreditation' => null,
                     'ID_Faculty' => $faculty->ID_Faculty,
                     'created_at' => now(),
